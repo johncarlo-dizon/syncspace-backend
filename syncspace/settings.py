9445813546303ec0269916231f1acd0e0c1bf4b8
johncarlo-dizon <johncarlovdizon@gmail.com>
2026-03-13 09:42:10 +0800

    conn 0

--- syncspace/settings.py
+++ syncspace/settings.py
@@ -95,7 +95,7 @@ DATABASE_URL = os.environ.get('DATABASE_URL')
 
 if DATABASE_URL:
     DATABASES = {
-        'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
+        'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=0)
     }
 else:
     DATABASES = {
